eba418a9386354b0dcc38ff6c4a2bebcfd9f74f1
spencercjh <shouspencercjh@foxmail.com>
2020-12-14 13:20:27 +0800

    217

--- src/main/java/spencercjh/problems/ContainsDuplicate.java
+++ src/main/java/spencercjh/problems/ContainsDuplicate.java
@@ -1,14 +1,16 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+import java.util.stream.IntStream;
+
 /**
  * https://leetcode-cn.com/problems/contains-duplicate/
  *
  * @author spencercjh
  */
 @Singleton
-public class ContainsDuplicate{
-
+public class ContainsDuplicate {
+  public boolean containsDuplicate(int[] nums) {
+    return IntStream.of(nums).distinct().count() != nums.length;
+  }
 }
-        
--- src/test/java/spencercjh/problems/ContainsDuplicateTest.java
+++ src/test/java/spencercjh/problems/ContainsDuplicateTest.java
@@ -1,10 +1,12 @@
 package spencercjh.problems;
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @MicronautTest
@@ -12,5 +14,10 @@ class ContainsDuplicateTest {
   @Inject
   private ContainsDuplicate solution;
 
+  @Test
+  void containsDuplicate() {
+    assertTrue(solution.containsDuplicate(new int[]{1, 2, 2, 3}));
+    assertFalse(solution.containsDuplicate(new int[]{1,2}));
+    assertFalse(solution.containsDuplicate(new int[]{1}));
+  }
 }
-        
