64e180327ee5673f64cebbd6698100bf23368e75
spencercjh <shouspencercjh@foxmail.com>
2020-12-14 13:42:39 +0800

    49

--- src/main/java/spencercjh/problems/GroupAnagrams.java
+++ src/main/java/spencercjh/problems/GroupAnagrams.java
@@ -1,14 +1,25 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+import java.util.*;
+
 /**
  * https://leetcode-cn.com/problems/group-anagrams/
  *
  * @author spencercjh
  */
 @Singleton
-public class GroupAnagrams{
-
+public class GroupAnagrams {
+  public List<List<String>> groupAnagrams(String[] strs) {
+    final Map<String, List<String>> keyResultMap = new HashMap<>();
+    for (String str : strs) {
+      final char[] strCharArray = str.toCharArray();
+      Arrays.sort(strCharArray);
+      final String key = new String(strCharArray);
+      final List<String> list = keyResultMap.getOrDefault(key, new ArrayList<>());
+      list.add(str);
+      keyResultMap.put(key, list);
+    }
+    return new ArrayList<>(keyResultMap.values());
+  }
 }
-        
