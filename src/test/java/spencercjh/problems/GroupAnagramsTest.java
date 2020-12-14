a80b8e0e4a7f19ade5a86ac4d707eca9bbd7443d
spencercjh <shouspencercjh@foxmail.com>
2020-12-14 13:42:39 +0800

    49

--- src/test/java/spencercjh/problems/GroupAnagramsTest.java
+++ src/test/java/spencercjh/problems/GroupAnagramsTest.java
@@ -1,10 +1,11 @@
 package spencercjh.problems;
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @MicronautTest
@@ -12,5 +13,8 @@ class GroupAnagramsTest {
   @Inject
   private GroupAnagrams solution;
 
+  @Test
+  void groupAnagrams() {
+    assertEquals(3, solution.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}).size());
+  }
 }
-        
