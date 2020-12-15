0f7375e88e5d50e072ef76fa79002b60a1958e00
spencercjh <shouspencercjh@foxmail.com>
2020-12-16 00:38:00 +0800

    738

--- src/main/java/spencercjh/problems/MonotoneIncreasingDigits.java
+++ src/main/java/spencercjh/problems/MonotoneIncreasingDigits.java
@@ -1,17 +1,27 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/monotone-increasing-digits/
  *
  * @author spencercjh
  */
 @Singleton
-public class MonotoneIncreasingDigits{
+public class MonotoneIncreasingDigits {
 
-    public int monotoneIncreasingDigits(int N) {
-
+  public int monotoneIncreasingDigits(int n) {
+    final char[] numbers = String.valueOf(n).toCharArray();
+    int nineBeginIndex = numbers.length;
+    for (int i = numbers.length - 1; i >= 1; i--) {
+      if (numbers[i] < numbers[i - 1]) {
+        nineBeginIndex = i;
+        numbers[i - 1]--;
+      }
     }
-
+    for (int i = nineBeginIndex; i < numbers.length; i++) {
+      numbers[i] = '9';
+    }
+    return Integer.parseInt(new String(numbers));
+  }
 }
--- src/test/java/spencercjh/problems/MonotoneIncreasingDigitsTest.java
+++ src/test/java/spencercjh/problems/MonotoneIncreasingDigitsTest.java
@@ -5,17 +5,21 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class MonotoneIncreasingDigitsTest {
 
   @Inject
   private MonotoneIncreasingDigits solution;
-  
+
   @Test
   void monotoneIncreasingDigits() {
-    
+    assertEquals(299, solution.monotoneIncreasingDigits(332));
+    assertEquals(9, solution.monotoneIncreasingDigits(10));
+    assertEquals(1234, solution.monotoneIncreasingDigits(1234));
+    assertEquals(4499, solution.monotoneIncreasingDigits(4532));
+    assertEquals(0, solution.monotoneIncreasingDigits(0));
+    assertEquals(1, solution.monotoneIncreasingDigits(1));
   }
-
 }
