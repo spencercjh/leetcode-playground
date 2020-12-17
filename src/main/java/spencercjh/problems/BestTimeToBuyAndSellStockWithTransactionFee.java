f27d97ade0ea00a987b4ae11fbbeef17ed66aa84
spencercjh <shouspencercjh@foxmail.com>
2020-12-17 14:05:12 +0800

    714

--- src/main/java/spencercjh/problems/BestTimeToBuyAndSellStockWithTransactionFee.java
+++ src/main/java/spencercjh/problems/BestTimeToBuyAndSellStockWithTransactionFee.java
@@ -1,17 +1,27 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
  *
  * @author spencercjh
  */
 @Singleton
-public class BestTimeToBuyAndSellStockWithTransactionFee{
+public class BestTimeToBuyAndSellStockWithTransactionFee {
 
-    public int maxProfit(int[] prices, int fee) {
-
+  public int maxProfit(int[] prices, int fee) {
+    int buy = prices[0] + fee;
+    int profit = 0;
+    for (int price : prices) {
+      if (price + fee < buy) {
+        buy = price + fee;
+      } else if (price > buy) {
+        profit += price - buy;
+        buy = price;
+      }
     }
+    return profit;
+  }
 
 }
--- src/test/java/spencercjh/problems/BestTimeToBuyAndSellStockWithTransactionFeeTest.java
+++ src/test/java/spencercjh/problems/BestTimeToBuyAndSellStockWithTransactionFeeTest.java
@@ -5,17 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class BestTimeToBuyAndSellStockWithTransactionFeeTest {
 
   @Inject
   private BestTimeToBuyAndSellStockWithTransactionFee solution;
-  
+
   @Test
   void maxProfit() {
-    
+    assertEquals(8, solution.maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
   }
 
 }
