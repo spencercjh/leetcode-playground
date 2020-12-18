2282883e281077d768f2240eb4584d18315b76aa
spencercjh <shouspencercjh@foxmail.com>
2020-12-18 08:17:02 +0800

    389

--- src/main/java/spencercjh/problems/FindTheDifference.java
+++ src/main/java/spencercjh/problems/FindTheDifference.java
@@ -1,17 +1,24 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/find-the-difference/
  *
  * @author spencercjh
  */
 @Singleton
-public class FindTheDifference{
+public class FindTheDifference {
 
-    public char findTheDifference(String s, String t) {
-
+  public char findTheDifference(String s, String t) {
+    char result = 0;
+    for (char ch : s.toCharArray()) {
+      result ^= ch;
     }
+    for (char ch : t.toCharArray()) {
+      result ^= ch;
+    }
+    return result;
+  }
 
 }
--- src/test/java/spencercjh/problems/FindTheDifferenceTest.java
+++ src/test/java/spencercjh/problems/FindTheDifferenceTest.java
@@ -5,17 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class FindTheDifferenceTest {
 
   @Inject
   private FindTheDifference solution;
-  
+
   @Test
   void findTheDifference() {
-    
+    assertEquals('a', solution.findTheDifference("aaa", "aaaa"));
+    assertEquals('b', solution.findTheDifference("a", "ab"));
   }
 
 }
