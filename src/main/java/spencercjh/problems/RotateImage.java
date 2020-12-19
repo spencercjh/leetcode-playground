897d27ef65628ee1aaeb26e41bf47d22ea8bcecf
spencercjh <shouspencercjh@foxmail.com>
2020-12-19 21:50:00 +0800

    48

--- src/main/java/spencercjh/problems/RotateImage.java
+++ src/main/java/spencercjh/problems/RotateImage.java
@@ -1,17 +1,27 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/rotate-image/
  *
  * @author spencercjh
  */
 @Singleton
-public class RotateImage{
+public class RotateImage {
 
-    public void rotate(int[][] matrix) {
-
+  public void rotate(int[][] matrix) {
+    // circle nums
+    for (int i = 0; i < matrix.length / 2; i++) {
+      // begin -> end row index
+      int end = matrix.length - 1 - i;
+      for (int j = 0; j < end - i; j++) {
+        int temp = matrix[i][i + j];
+        matrix[i][i + j] = matrix[end - j][i];
+        matrix[end - j][i] = matrix[end][end - j];
+        matrix[end][end - j] = matrix[i + j][end];
+        matrix[i + j][end] = temp;
+      }
     }
-
+  }
 }
--- src/test/java/spencercjh/problems/RotateImageTest.java
+++ src/test/java/spencercjh/problems/RotateImageTest.java
@@ -1,21 +1,36 @@
 package spencercjh.problems;
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 @MicronautTest
 class RotateImageTest {
 
   @Inject
   private RotateImage solution;
-  
+
   @Test
-  void rotate() {
-    
+  void rotate1() {
+    final int[][] input = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    solution.rotate(input);
+    final int[][] expect = {{7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
+    IntStream.range(0, input.length)
+      .mapToObj(i -> Arrays.equals(expect[i], input[i]))
+      .forEach(Assertions::assertTrue);
   }
 
+  @Test
+  void rotate2() {
+    final int[][] input = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+    solution.rotate(input);
+    final int[][] expect = {{13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}, {16, 12, 8, 4}};
+    IntStream.range(0, input.length)
+      .mapToObj(i -> Arrays.equals(expect[i], input[i]))
+      .forEach(Assertions::assertTrue);
+  }
 }
