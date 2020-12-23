8e7cf8165b486c7da7138e841bd55f256c87bc26
spencercjh <shouspencercjh@foxmail.com>
2020-12-23 13:47:41 +0800

    387

--- src/main/java/spencercjh/problems/FirstUniqueCharacterInAString.java
+++ src/main/java/spencercjh/problems/FirstUniqueCharacterInAString.java
@@ -1,17 +1,26 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/first-unique-character-in-a-string/
  *
  * @author spencercjh
  */
+@SuppressWarnings("AlibabaClassNamingShouldBeCamel")
 @Singleton
-public class FirstUniqueCharacterInAString{
+public class FirstUniqueCharacterInAString {
 
-    public int firstUniqChar(String s) {
-
+  public int firstUniqChar(String s) {
+    int result = Integer.MAX_VALUE;
+    for (char ch = 'a'; ch <= 'z'; ch++) {
+      final int left = s.indexOf(ch);
+      final int right = s.lastIndexOf(ch);
+      if (left == right && left != -1) {
+        result = Math.min(result, left);
+      }
     }
+    return result != Integer.MAX_VALUE ? result : -1;
+  }
 
 }
--- src/test/java/spencercjh/problems/FirstUniqueCharacterInAStringTest.java
+++ src/test/java/spencercjh/problems/FirstUniqueCharacterInAStringTest.java
@@ -5,17 +5,19 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class FirstUniqueCharacterInAStringTest {
 
   @Inject
   private FirstUniqueCharacterInAString solution;
-  
+
   @Test
   void firstUniqChar() {
-    
+    assertEquals(0, solution.firstUniqChar("leetcode"));
+    assertEquals(2, solution.firstUniqChar("loveleetcode"));
+    assertEquals(-1, solution.firstUniqChar("aabbcc"));
   }
 
 }
