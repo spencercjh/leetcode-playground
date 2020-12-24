c5805a5bf12f4f9cc310f99091e04d9f90495e24
spencercjh <shouspencercjh@foxmail.com>
2020-12-24 13:29:28 +0800

    135

--- src/main/java/spencercjh/problems/Candy.java
+++ src/main/java/spencercjh/problems/Candy.java
@@ -1,17 +1,34 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/candy/
  *
  * @author spencercjh
  */
 @Singleton
-public class Candy{
+public class Candy {
 
-    public int candy(int[] ratings) {
-
+  public int candy(int[] ratings) {
+    final int[] leftResult = new int[ratings.length];
+    for (int i = 0; i < ratings.length; i++) {
+      if (i > 0 && ratings[i - 1] < ratings[i]) {
+        leftResult[i] = leftResult[i - 1] + 1;
+      } else {
+        leftResult[i] = 1;
+      }
     }
+    int result = 0, right = 1;
+    for (int i = ratings.length - 1; i >= 0; i--) {
+      if (i < ratings.length - 1 && ratings[i] > ratings[i + 1]) {
+        right++;
+      } else {
+        right = 1;
+      }
+      result += Math.max(leftResult[i], right);
+    }
+    return result;
+  }
 
 }
--- src/test/java/spencercjh/problems/CandyTest.java
+++ src/test/java/spencercjh/problems/CandyTest.java
@@ -5,17 +5,20 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class CandyTest {
 
   @Inject
   private Candy solution;
-  
+
   @Test
   void candy() {
-    
+    assertEquals(3, solution.candy(new int[]{1, 1, 1}));
+    assertEquals(5, solution.candy(new int[]{1, 0, 2}));
+    assertEquals(4, solution.candy(new int[]{1, 2, 2}));
+    assertEquals(13, solution.candy(new int[]{1, 2, 87, 87, 87, 2, 1}));
   }
 
 }
