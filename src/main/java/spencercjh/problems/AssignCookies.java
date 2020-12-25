5d39b0eba3b29a219141b905a8c1ea9091aa90f8
spencercjh <shouspencercjh@foxmail.com>
2020-12-25 15:02:59 +0800

    455

--- src/main/java/spencercjh/problems/AssignCookies.java
+++ src/main/java/spencercjh/problems/AssignCookies.java
@@ -1,17 +1,29 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+import java.util.Arrays;
+
 /**
  * https://leetcode-cn.com/problems/assign-cookies/
  *
  * @author spencercjh
  */
 @Singleton
-public class AssignCookies{
+public class AssignCookies {
 
-    public int findContentChildren(int[] g, int[] s) {
-
+  public int findContentChildren(int[] g, int[] s) {
+    Arrays.sort(g);
+    Arrays.sort(s);
+    int result = 0;
+    for (int i = 0, j = 0; i < g.length && j < s.length; i++, j++) {
+      while (j < s.length && s[j] < g[i]) {
+        j++;
+      }
+      if (j < s.length) {
+        result++;
+      }
     }
+    return result;
+  }
 
 }
--- src/test/java/spencercjh/problems/AssignCookiesTest.java
+++ src/test/java/spencercjh/problems/AssignCookiesTest.java
@@ -5,17 +5,19 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class AssignCookiesTest {
 
   @Inject
   private AssignCookies solution;
-  
+
   @Test
   void findContentChildren() {
-    
+    assertEquals(2, solution.findContentChildren(new int[]{1, 2}, new int[]{1, 2, 3}));
+    assertEquals(1, solution.findContentChildren(new int[]{1, 2, 3}, new int[]{1, 1}));
+    assertEquals(2, solution.findContentChildren(new int[]{10, 9, 8, 7}, new int[]{5, 6, 7, 8}));
   }
 
 }
