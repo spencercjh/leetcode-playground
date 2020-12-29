75284a59c60005fe2189557edc257cc3f7fa41d9
spencercjh <shouspencercjh@foxmail.com>
2020-12-29 16:15:25 +0800

    188

--- src/main/java/spencercjh/problems/BestTimeToBuyAndSellStockIv.java
+++ src/main/java/spencercjh/problems/BestTimeToBuyAndSellStockIv.java
@@ -1,6 +1,7 @@
 package spencercjh.problems;
 
 import javax.inject.Singleton;
+import java.util.Arrays;
 
 /**
  * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iv/
@@ -11,7 +12,27 @@ import javax.inject.Singleton;
 public class BestTimeToBuyAndSellStockIv {
 
   public int maxProfit(int k, int[] prices) {
-    return 0;
-  }
+    if (prices.length == 0) {
+      return 0;
+    }
+    k = Math.min(k, prices.length / 2);
+    final int[] buy = new int[k + 1];
+    final int[] sell = new int[k + 1];
 
+    buy[0] = -prices[0];
+    sell[0] = 0;
+    for (int i = 1; i <= k; ++i) {
+      buy[i] = sell[i] = Integer.MIN_VALUE / 2;
+    }
+
+    for (int i = 1; i < prices.length; ++i) {
+      buy[0] = Math.max(buy[0], sell[0] - prices[i]);
+      for (int j = 1; j <= k; ++j) {
+        buy[j] = Math.max(buy[j], sell[j] - prices[i]);
+        sell[j] = Math.max(sell[j], buy[j - 1] + prices[i]);
+      }
+    }
+
+    return Arrays.stream(sell).max().getAsInt();
+  }
 }
--- src/test/java/spencercjh/problems/BestTimeToBuyAndSellStockIvTest.java
+++ src/test/java/spencercjh/problems/BestTimeToBuyAndSellStockIvTest.java
@@ -5,17 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class BestTimeToBuyAndSellStockIvTest {
 
   @Inject
   private BestTimeToBuyAndSellStockIv solution;
-  
+
   @Test
   void maxProfit() {
-    
+    assertEquals(2, solution.maxProfit(2, new int[]{2, 4, 1}));
+    assertEquals(7, solution.maxProfit(2, new int[]{3, 2, 6, 5, 0, 3}));
   }
-
 }
