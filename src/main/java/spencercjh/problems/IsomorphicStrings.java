c317e76d2d3240baa9cb0cf8746f50d7a3f00510
spencercjh <shouspencercjh@foxmail.com>
2020-12-29 15:11:01 +0800

    205

--- src/main/java/spencercjh/problems/IsomorphicStrings.java
+++ src/main/java/spencercjh/problems/IsomorphicStrings.java
@@ -11,6 +11,26 @@ import javax.inject.Singleton;
 public class IsomorphicStrings {
 
   public boolean isIsomorphic(String s, String t) {
+    if (s == null || t == null || s.length() != t.length()) {
+      return false;
+    }
+    final char[] charArrayS = s.toCharArray();
+    final char[] charArrayT = t.toCharArray();
+    final char[] characterMapping = new char[255];
+    final boolean[] isCharacterMapped = new boolean[255];
+    for (int i = 0; i < charArrayS.length; i++) {
+      final char characterS = charArrayS[i];
+      final char characterT = charArrayT[i];
+      if (characterMapping[characterS] == 0) {
+        if (isCharacterMapped[characterT]) {
+          return false;
+        }
+        characterMapping[characterS] = characterT;
+        isCharacterMapped[characterT] = true;
+      } else if (characterMapping[characterS] != characterT) {
+        return false;
+      }
+    }
     return true;
   }
 
--- src/test/java/spencercjh/problems/IsomorphicStringsTest.java
+++ src/test/java/spencercjh/problems/IsomorphicStringsTest.java
@@ -5,17 +5,24 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @MicronautTest
 class IsomorphicStringsTest {
 
   @Inject
   private IsomorphicStrings solution;
-  
+
   @Test
   void isIsomorphic() {
-    
+    assertTrue(solution.isIsomorphic("egg", "add"));
+    assertTrue(solution.isIsomorphic("egg", "tdd"));
+    assertTrue(solution.isIsomorphic("a", "a"));
+    assertTrue(solution.isIsomorphic("a", "b"));
+    assertTrue(solution.isIsomorphic("", ""));
+    assertFalse(solution.isIsomorphic("ab", "aa"));
+    assertTrue(solution.isIsomorphic("paper", "title"));
+    assertFalse(solution.isIsomorphic("foo", "bar"));
   }
-
 }
