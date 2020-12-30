d0b3e88a35f675533f1e8474b17512351b559cbf
spencercjh <shouspencercjh@foxmail.com>
2020-12-30 13:37:10 +0800

    1046

--- src/main/java/spencercjh/problems/LastStoneWeight.java
+++ src/main/java/spencercjh/problems/LastStoneWeight.java
@@ -1,17 +1,32 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+import java.util.Arrays;
+
 /**
  * https://leetcode-cn.com/problems/last-stone-weight/
  *
  * @author spencercjh
  */
 @Singleton
-public class LastStoneWeight{
+public class LastStoneWeight {
 
-    public int lastStoneWeight(int[] stones) {
-        
+  public int lastStoneWeight(int[] stones) {
+    if (stones == null || stones.length == 0) {
+      return 0;
     }
+    if (stones.length == 1) {
+      return stones[0];
+    }
+    Arrays.sort(stones);
+    while (stones[stones.length - 2] != 0) {
+      final int max = stones[stones.length - 1];
+      final int smaller = stones[stones.length - 2];
+      stones[stones.length - 1] = max - smaller;
+      stones[stones.length - 2] = 0;
+      Arrays.sort(stones);
+    }
+    return stones[stones.length - 1];
+  }
 
 }
--- src/test/java/spencercjh/problems/LastStoneWeightTest.java
+++ src/test/java/spencercjh/problems/LastStoneWeightTest.java
@@ -5,17 +5,19 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class LastStoneWeightTest {
 
   @Inject
   private LastStoneWeight solution;
-  
+
   @Test
   void lastStoneWeight() {
-    
+    assertEquals(1, solution.lastStoneWeight(new int[]{2, 7, 4, 1, 8, 1}));
+    assertEquals(1, solution.lastStoneWeight(new int[]{1}));
+    assertEquals(0, solution.lastStoneWeight(new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}));
   }
 
 }
