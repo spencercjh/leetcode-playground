008dcb7aa431f21d82f82cac9c18f09ec3ca8b78
spencercjh <shouspencercjh@foxmail.com>
2020-12-31 09:57:18 +0800

    435

--- src/main/java/spencercjh/problems/NonOverlappingIntervals.java
+++ src/main/java/spencercjh/problems/NonOverlappingIntervals.java
@@ -1,17 +1,37 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+import java.util.Arrays;
+
 /**
  * https://leetcode-cn.com/problems/non-overlapping-intervals/
  *
  * @author spencercjh
  */
 @Singleton
-public class NonOverlappingIntervals{
+public class NonOverlappingIntervals {
 
-    public int eraseOverlapIntervals(int[][] intervals) {
-
+  public int eraseOverlapIntervals(int[][] intervals) {
+    if (intervals == null || intervals.length <= 1) {
+      return 0;
     }
+    Arrays.sort(intervals, (array1, array2) -> {
+      if (array1[0] != array2[0]) {
+        return array1[0] - array2[0];
+      } else {
+        return array1[1] - array2[1];
+      }
+    });
+    int count = 0;
+    for (int i = 0, j = 1; i < intervals.length && j < intervals.length; i++, j++) {
+      if (intervals[i][1] > intervals[j][0]) {
+        i = intervals[i][1] < intervals[j][1] ? i - 1 : j - 1;
+        count++;
+      } else {
+        i = j - 1;
+      }
+    }
+    return count;
+  }
 
 }
--- src/test/java/spencercjh/problems/NonOverlappingIntervalsTest.java
+++ src/test/java/spencercjh/problems/NonOverlappingIntervalsTest.java
@@ -5,17 +5,20 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class NonOverlappingIntervalsTest {
 
   @Inject
   private NonOverlappingIntervals solution;
-  
+
   @Test
   void eraseOverlapIntervals() {
-    
+    assertEquals(1, solution.eraseOverlapIntervals(new int[][]{{1, 2}, {2, 3}, {3, 4}, {1, 3}}));
+    assertEquals(2, solution.eraseOverlapIntervals(new int[][]{{1, 2}, {1, 3}, {1, 5}, {2, 4}}));
+    assertEquals(2, solution.eraseOverlapIntervals(new int[][]{{1, 2}, {1, 2}, {1, 2}}));
+    assertEquals(0, solution.eraseOverlapIntervals(new int[][]{{1, 2}, {2, 3}}));
+    assertEquals(0, solution.eraseOverlapIntervals(new int[][]{{1, 2}}));
   }
-
 }
