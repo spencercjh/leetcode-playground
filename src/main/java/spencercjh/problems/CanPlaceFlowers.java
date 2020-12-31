fc72ae13d6e6e4952ef4a0b62f165259cc67dd6b
spencercjh <shouspencercjh@foxmail.com>
2021-01-01 03:02:30 +0800

    605

--- src/main/java/spencercjh/problems/CanPlaceFlowers.java
+++ src/main/java/spencercjh/problems/CanPlaceFlowers.java
@@ -1,17 +1,30 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/can-place-flowers/
  *
  * @author spencercjh
  */
 @Singleton
-public class CanPlaceFlowers{
+public class CanPlaceFlowers {
 
-    public boolean canPlaceFlowers(int[] flowerbed, int n) {
-
+  public boolean canPlaceFlowers(int[] flowerbed, int n) {
+    for (int i = 0; i < flowerbed.length && n > 0; ) {
+      // there is a flower in the current flowerpot
+      if (flowerbed[i] == 1) {
+        i += 2;
+      }
+      // Both current and next flowerpots are empty, can place one
+      else if (flowerbed[i] == 0 && (i + 1 >= flowerbed.length || flowerbed[i + 1] == 0)) {
+        i += 2;
+        n--;
+      } else {
+        i++;
+      }
     }
+    return n == 0;
+  }
 
 }
--- src/test/java/spencercjh/problems/CanPlaceFlowersTest.java
+++ src/test/java/spencercjh/problems/CanPlaceFlowersTest.java
@@ -5,17 +5,19 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @MicronautTest
 class CanPlaceFlowersTest {
 
   @Inject
   private CanPlaceFlowers solution;
-  
+
   @Test
   void canPlaceFlowers() {
-    
+    assertTrue(solution.canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 1));
+    assertFalse(solution.canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 2));
+    assertTrue(solution.canPlaceFlowers(new int[]{0, 0, 1, 0, 0}, 1));
   }
-
 }
