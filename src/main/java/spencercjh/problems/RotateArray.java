eff970cde2a1e0b618387ad97eb5da640ffaaa87
spencercjh <shouspencercjh@foxmail.com>
2021-01-08 10:45:53 +0800

    189

--- src/main/java/spencercjh/problems/RotateArray.java
+++ src/main/java/spencercjh/problems/RotateArray.java
@@ -1,17 +1,33 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/rotate-array/
  *
  * @author spencercjh
  */
 @Singleton
-public class RotateArray{
+public class RotateArray {
 
-    public void rotate(int[] nums, int k) {
-
+  public void rotate(int[] nums, int k) {
+    k = k % nums.length;
+    for (int startIndex = 0; startIndex < gcd(k, nums.length); startIndex++) {
+      int currentIndex = startIndex;
+      int currentNum = nums[currentIndex];
+      do {
+        final int nextIndex = (currentIndex + k) % nums.length;
+        final int temp = nums[nextIndex];
+        nums[nextIndex] = currentNum;
+        currentNum = temp;
+        currentIndex = nextIndex;
+      } while (startIndex != currentIndex);
     }
+  }
 
+
+  private int gcd(int x, int y) {
+    //noinspection SuspiciousNameCombination
+    return y > 0 ? gcd(y, x % y) : x;
+  }
 }
--- src/test/java/spencercjh/problems/RotateArrayTest.java
+++ src/test/java/spencercjh/problems/RotateArrayTest.java
@@ -5,17 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static spencercjh.AssertionUtils.assertArrayEquals;
 
 @MicronautTest
 class RotateArrayTest {
 
   @Inject
   private RotateArray solution;
-  
+
   @Test
   void rotate() {
-    
+    final int[] nums = {1, 2, 3, 4, 5, 6, 7};
+    solution.rotate(nums, 3);
+    assertArrayEquals(new int[]{5, 6, 7, 1, 2, 3, 4}, nums);
   }
-
 }
