aef0324c2b07aac69b10cd902c51b8106fa04b7c
spencercjh <shouspencercjh@foxmail.com>
2021-01-09 18:43:01 +0800

    123

--- src/main/java/spencercjh/problems/BestTimeToBuyAndSellStockIii.java
+++ src/main/java/spencercjh/problems/BestTimeToBuyAndSellStockIii.java
@@ -1,17 +1,25 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iii/
  *
  * @author spencercjh
  */
 @Singleton
-public class BestTimeToBuyAndSellStockIii{
+public class BestTimeToBuyAndSellStockIii {
 
-    public int maxProfit(int[] prices) {
-
+  public int maxProfit(int[] prices) {
+    int buy1 = -prices[0], sell1 = 0;
+    int buy2 = -prices[0], sell2 = 0;
+    for (int i = 1; i < prices.length; ++i) {
+      buy1 = Math.max(buy1, -prices[i]);
+      sell1 = Math.max(sell1, buy1 + prices[i]);
+      buy2 = Math.max(buy2, sell1 - prices[i]);
+      sell2 = Math.max(sell2, buy2 + prices[i]);
     }
+    return sell2;
+  }
 
 }
--- src/test/java/spencercjh/problems/BestTimeToBuyAndSellStockIiiTest.java
+++ src/test/java/spencercjh/problems/BestTimeToBuyAndSellStockIiiTest.java
@@ -5,17 +5,19 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class BestTimeToBuyAndSellStockIiiTest {
 
   @Inject
   private BestTimeToBuyAndSellStockIii solution;
-  
+
   @Test
   void maxProfit() {
-    
+    assertEquals(6, solution.maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4}));
+    assertEquals(4, solution.maxProfit(new int[]{1, 2, 3, 4, 5}));
+    assertEquals(0, solution.maxProfit(new int[]{7, 6, 4, 3, 1}));
+    assertEquals(0, solution.maxProfit(new int[]{0}));
   }
-
 }
