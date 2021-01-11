af922f6a169f0e0d1f16ae4f86cf4d28e3bb79aa
spencercjh <shouspencercjh@foxmail.com>
2021-01-11 14:47:33 +0800

    1202

--- src/main/java/spencercjh/problems/SmallestStringWithSwaps.java
+++ src/main/java/spencercjh/problems/SmallestStringWithSwaps.java
@@ -1,7 +1,10 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 /**
  * https://leetcode-cn.com/problems/smallest-string-with-swaps/
@@ -9,10 +12,61 @@ import java.util.List;
  * @author spencercjh
  */
 @Singleton
-public class SmallestStringWithSwaps{
+public class SmallestStringWithSwaps {
 
-    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
-        return "";
+  private int[] parents;
+
+  public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+    if (pairs.size() == 0) {
+      return s;
     }
 
+    parents = new int[s.length()];
+    for (int i = 0; i < parents.length; i++) {
+      parents[i] = i;
+    }
+
+    for (List<Integer> pair : pairs) {
+      final int x = pair.get(0);
+      final int y = pair.get(1);
+      union(x, y);
+    }
+
+    final Map<Integer, PriorityQueue<Character>> map = new HashMap<>(s.length());
+    final char[] chars = s.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      final int root = find(i);
+
+      if (!map.containsKey(root)) {
+        final PriorityQueue<Character> heap = new PriorityQueue<>();
+        heap.offer(chars[i]);
+        map.put(root, heap);
+      } else {
+        map.get(root).offer(chars[i]);
+      }
+    }
+
+    final StringBuilder result = new StringBuilder();
+    for (int i = 0; i < chars.length; i++) {
+      result.append(map.get(find(i)).poll());
+    }
+
+    return result.toString();
+  }
+
+  private void union(int x, int y) {
+    final int rootX = find(x);
+    final int rootY = find(y);
+    if (rootX == rootY) {
+      return;
+    }
+    parents[rootX] = rootY;
+  }
+
+  private int find(int node) {
+    if (node != parents[node]) {
+      parents[node] = find(parents[node]);
+    }
+    return parents[node];
+  }
 }
--- src/test/java/spencercjh/problems/SmallestStringWithSwapsTest.java
+++ src/test/java/spencercjh/problems/SmallestStringWithSwapsTest.java
@@ -4,18 +4,23 @@ import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class SmallestStringWithSwapsTest {
 
   @Inject
   private SmallestStringWithSwaps solution;
-  
+
   @Test
   void smallestStringWithSwaps() {
-    
+    assertEquals("abcd", solution.smallestStringWithSwaps("dcab",
+      List.of(List.of(0, 3), List.of(1, 2), List.of(0, 2))));
+
+    assertEquals("abc", solution.smallestStringWithSwaps("cba",
+      List.of(List.of(0, 1), List.of(1, 2))));
   }
 
 }
