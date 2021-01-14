33b8c857213347c81659e776942baff4c2595b0b
spencercjh <shouspencercjh@foxmail.com>
2021-01-14 23:20:35 +0800

    1018

--- src/main/java/spencercjh/problems/BinaryPrefixDivisibleBy5.java
+++ src/main/java/spencercjh/problems/BinaryPrefixDivisibleBy5.java
@@ -1,17 +1,34 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * https://leetcode-cn.com/problems/binary-prefix-divisible-by-5/
  *
  * @author spencercjh
  */
 @Singleton
-public class BinaryPrefixDivisibleBy5{
+public class BinaryPrefixDivisibleBy5 {
 
-    public List<Boolean> prefixesDivBy5(int[] A) {
-
+  @SuppressWarnings("AlibabaLowerCamelCaseVariableNaming")
+  public List<Boolean> prefixesDivBy5(int[] A) {
+    if (A == null || A.length == 0) {
+      return Collections.emptyList();
     }
+    final List<Boolean> answers = new ArrayList<>(A.length);
+    for (int i = 0, num = 0; i < A.length; i++) {
+      // 为prefix进位
+      num <<= 1;
+      // 加上个位数字
+      num += A[i];
+      // 最后只判断个位数字
+      num %= 10;
+      answers.add(num % 5 == 0);
+    }
+    return answers;
+  }
 
 }
--- src/test/java/spencercjh/problems/BinaryPrefixDivisibleBy5Test.java
+++ src/test/java/spencercjh/problems/BinaryPrefixDivisibleBy5Test.java
@@ -2,20 +2,23 @@ package spencercjh.problems;
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
+import spencercjh.Utils;
 
 import javax.inject.Inject;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @MicronautTest
 class BinaryPrefixDivisibleBy5Test {
 
   @Inject
   private BinaryPrefixDivisibleBy5 solution;
-  
+
   @Test
   void prefixesDivBy5() {
-    
+    assertTrue(Utils.equals(List.of(true, false, false), solution.prefixesDivBy5(new int[]{0, 1, 1})));
+    assertTrue(Utils.equals(List.of(false, false, false), solution.prefixesDivBy5(new int[]{1, 1, 1})));
   }
 
 }
