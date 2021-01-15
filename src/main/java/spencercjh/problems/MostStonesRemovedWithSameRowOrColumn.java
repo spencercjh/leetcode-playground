53d094f34e632986a4b6d38c70e361e21617876f
spencercjh <shouspencercjh@foxmail.com>
2021-01-15 10:57:18 +0800

    947

--- src/main/java/spencercjh/problems/MostStonesRemovedWithSameRowOrColumn.java
+++ src/main/java/spencercjh/problems/MostStonesRemovedWithSameRowOrColumn.java
@@ -1,17 +1,52 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode-cn.com/problems/most-stones-removed-with-same-row-or-column/
  *
  * @author spencercjh
  */
 @Singleton
-public class MostStonesRemovedWithSameRowOrColumn{
+public class MostStonesRemovedWithSameRowOrColumn {
 
-    public int removeStones(int[][] stones) {
+  private Map<Integer, Integer> parents;
+  private int count = 0;
 
+  public int removeStones(int[][] stones) {
+    if (stones == null || stones.length <= 1) {
+      return 0;
+    }
+    parents = new HashMap<>((int) ((2 * stones.length) / 0.75));
+    for (int[] stone : stones) {
+      union(~stone[0], stone[1]);
+    }
+    return stones.length - count;
+  }
+
+
+  private int find(int n) {
+    if (!parents.containsKey(n)) {
+      parents.put(n, n);
+      count++;
     }
 
+    if (n != parents.get(n)) {
+      parents.put(n, find(parents.get(n)));
+    }
+
+    return parents.get(n);
+  }
+
+  private void union(int x, int y) {
+    final int rootX = find(x);
+    final int rootY = find(y);
+    if (rootX == rootY) {
+      return;
+    }
+    parents.put(rootX, rootY);
+    count--;
+  }
 }
--- src/test/java/spencercjh/problems/MostStonesRemovedWithSameRowOrColumnTest.java
+++ src/test/java/spencercjh/problems/MostStonesRemovedWithSameRowOrColumnTest.java
@@ -5,17 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class MostStonesRemovedWithSameRowOrColumnTest {
 
   @Inject
   private MostStonesRemovedWithSameRowOrColumn solution;
-  
+
   @Test
   void removeStones() {
-    
+    assertEquals(5, solution.removeStones(new int[][]{{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}}));
   }
 
 }
