4c24aa9c2c441bf5bd8714823133d7f580f4e788
spencercjh <shouspencercjh@foxmail.com>
2021-01-19 18:57:11 +0800

    1584

--- src/main/java/spencercjh/problems/MinCostToConnectAllPoints.java
+++ src/main/java/spencercjh/problems/MinCostToConnectAllPoints.java
@@ -1,6 +1,9 @@
 package spencercjh.problems;
 
 import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * https://leetcode-cn.com/problems/min-cost-to-connect-all-points/
@@ -10,8 +13,68 @@ import javax.inject.Singleton;
 @Singleton
 public class MinCostToConnectAllPoints {
 
+  private int[] parents;
+  private int[][] points;
+
   public int minCostConnectPoints(int[][] points) {
-    return 0;
+    if (points == null || points.length <= 1) {
+      return 0;
+    }
+
+    this.points = points;
+    parents = new int[points.length];
+    final List<Edge> edges = new ArrayList<>(points.length * points.length);
+    for (int i = 0; i < parents.length; i++) {
+      parents[i] = i;
+    }
+
+    for (int i = 0; i < points.length; i++) {
+      for (int j = i + 1; j < points.length; j++) {
+        edges.add(new Edge(i, j));
+      }
+    }
+    edges.sort(Comparator.comparingInt(edge -> edge.length));
+
+    int n = 1;
+    int answer = 0;
+    for (Edge edge : edges) {
+      if (union(edge.x, edge.y)) {
+        answer += edge.length;
+        if (++n == points.length) {
+          break;
+        }
+      }
+    }
+    return answer;
   }
 
+  private int find(int n) {
+    if (n != parents[n]) {
+      parents[n] = find(parents[n]);
+    }
+    return parents[n];
+  }
+
+  private boolean union(int x, int y) {
+    final int rootX = find(x);
+    final int rootY = find(y);
+    if (rootX == rootY) {
+      return false;
+    }
+    parents[rootX] = rootY;
+    return true;
+  }
+
+  private class Edge {
+    final int x;
+    final int y;
+    final int length;
+
+    private Edge(int x, int y) {
+      this.x = x;
+      this.y = y;
+      // |xi - xj| + |yi - yj|
+      this.length = Math.abs(points[x][0] - points[y][0]) + Math.abs(points[x][1] - points[y][1]);
+    }
+  }
 }
--- src/test/java/spencercjh/problems/MinCostToConnectAllPointsTest.java
+++ src/test/java/spencercjh/problems/MinCostToConnectAllPointsTest.java
@@ -5,17 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class MinCostToConnectAllPointsTest {
 
   @Inject
   private MinCostToConnectAllPoints solution;
-  
+
   @Test
   void minCostConnectPoints() {
-    
+    assertEquals(20, solution.minCostConnectPoints(new int[][]{{0, 0}, {2, 2}, {3, 10}, {5, 2}, {7, 0}}));
   }
 
 }
