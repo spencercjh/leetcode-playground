107955468767fc95f959d28255d414d77392e6ab
spencercjh <shouspencercjh@foxmail.com>
2021-01-20 09:32:28 +0800

    628

--- src/main/java/spencercjh/problems/MaximumProductOfThreeNumbers.java
+++ src/main/java/spencercjh/problems/MaximumProductOfThreeNumbers.java
@@ -1,17 +1,38 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/maximum-product-of-three-numbers/
  *
  * @author spencercjh
  */
 @Singleton
-public class MaximumProductOfThreeNumbers{
+public class MaximumProductOfThreeNumbers {
 
-    public int maximumProduct(int[] nums) {
+  public int maximumProduct(int[] nums) {
+    int max = Integer.MIN_VALUE, nextMax = Integer.MIN_VALUE, nextNextMax = Integer.MIN_VALUE,
+      min = Integer.MAX_VALUE, nextMin = Integer.MAX_VALUE;
+    for (int num : nums) {
+      if (num < min) {
+        nextMin = min;
+        min = num;
+      } else if (num < nextMin) {
+        nextMin = num;
+      }
 
+      if (num > max) {
+        nextNextMax = nextMax;
+        nextMax = max;
+        max = num;
+      } else if (num > nextMax) {
+        nextNextMax = nextMax;
+        nextMax = num;
+      } else if (num > nextNextMax) {
+        nextNextMax = num;
+      }
     }
-
+    // 输入的数组中任意三个数的乘积不会超出32位有符号整数的范围。
+    return Math.max(nextMin * min * max, max * nextMax * nextNextMax);
+  }
 }
--- src/test/java/spencercjh/problems/MaximumProductOfThreeNumbersTest.java
+++ src/test/java/spencercjh/problems/MaximumProductOfThreeNumbersTest.java
@@ -5,17 +5,19 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class MaximumProductOfThreeNumbersTest {
 
   @Inject
   private MaximumProductOfThreeNumbers solution;
-  
+
   @Test
   void maximumProduct() {
-    
+    assertEquals(6, solution.maximumProduct(new int[]{1, 2, 3}));
+    assertEquals(24, solution.maximumProduct(new int[]{1, 2, 3, 4}));
+    assertEquals(24, solution.maximumProduct(new int[]{1, 2, -3, -4}));
   }
 
 }
