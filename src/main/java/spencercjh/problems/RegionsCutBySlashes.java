88296f10ebf7b9204d55c01253aa54850aea5281
spencercjh <shouspencercjh@foxmail.com>
2021-01-26 15:21:31 +0800

    959

--- src/main/java/spencercjh/problems/RegionsCutBySlashes.java
+++ src/main/java/spencercjh/problems/RegionsCutBySlashes.java
@@ -9,9 +9,80 @@ import javax.inject.Singleton;
  */
 @Singleton
 public class RegionsCutBySlashes {
+  private int[] parent;
+  private int count;
 
+  /**
+   * ------------------
+   * |   0   ||   0   |
+   * |3     1||3     1|
+   * |   2   ||   2   |
+   * ------------------
+   * ------------------
+   * |   0   ||   0   |
+   * |3     1||3     1|
+   * |   2   ||   2   |
+   * ------------------
+   */
   public int regionsBySlashes(String[] grid) {
-    return 0;
+    final int size = 4 * grid.length * grid.length;
+
+    count = size;
+    parent = new int[size];
+    for (int i = 0; i < size; i++) {
+      parent[i] = i;
+    }
+
+    for (int i = 0; i < grid.length; i++) {
+      final char[] row = grid[i].toCharArray();
+      for (int j = 0; j < row.length; j++) {
+        final int index = 4 * (i * row.length + j);
+        // inside grid
+        switch (row[j]) {
+          case '/':
+            // 0-3 and 1-2
+            union(index, index + 3);
+            union(index + 1, index + 2);
+            break;
+          case '\\':
+            // 0-1 and 2-3
+            union(index, index + 1);
+            union(index + 2, index + 3);
+            break;
+          default:
+            // 0-1-2-3
+            union(index, index + 1);
+            union(index + 1, index + 2);
+            union(index + 2, index + 3);
+            break;
+        }
+
+        // adjacent grid 0-2 and 1-3
+        if (j + 1 < grid.length) {
+          union(index + 1, 4 * (i * row.length + j + 1) + 3);
+        }
+        if (i + 1 < grid.length) {
+          union(index + 2, 4 * ((i + 1) * row.length + j));
+        }
+      }
+    }
+    return count;
   }
 
+  private int find(int n) {
+    if (n != parent[n]) {
+      parent[n] = find(parent[n]);
+    }
+    return parent[n];
+  }
+
+  private void union(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x == y) {
+      return;
+    }
+    parent[x] = y;
+    count--;
+  }
 }
--- src/test/java/spencercjh/problems/RegionsCutBySlashesTest.java
+++ src/test/java/spencercjh/problems/RegionsCutBySlashesTest.java
@@ -5,17 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class RegionsCutBySlashesTest {
 
   @Inject
   private RegionsCutBySlashes solution;
-  
+
   @Test
   void regionsBySlashes() {
-    
+    assertEquals(2, solution.regionsBySlashes(new String[]{" /", "/ "}));
+    assertEquals(3, solution.regionsBySlashes(new String[]{"//", "/ "}));
   }
 
 }
