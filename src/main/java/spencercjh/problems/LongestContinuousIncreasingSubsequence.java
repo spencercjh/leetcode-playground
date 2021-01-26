e419419248bc08da13ff18f40f900f7625f33a85
spencercjh <shouspencercjh@foxmail.com>
2021-01-26 14:20:29 +0800

    674

--- src/main/java/spencercjh/problems/LongestContinuousIncreasingSubsequence.java
+++ src/main/java/spencercjh/problems/LongestContinuousIncreasingSubsequence.java
@@ -10,8 +10,24 @@ import javax.inject.Singleton;
 @Singleton
 public class LongestContinuousIncreasingSubsequence {
 
+  @SuppressWarnings("AlibabaLowerCamelCaseVariableNaming")
   public int findLengthOfLCIS(int[] nums) {
-    return 0;
+    if (nums == null) {
+      return 0;
+    }
+    if (nums.length <= 1) {
+      return nums.length;
+    }
+
+    int ans = 0;
+    for (int i = 0, start = 0; i < nums.length; i++) {
+      // restart
+      if (i > 0 && nums[i] <= nums[i - 1]) {
+        start = i;
+      }
+      ans = Math.max(ans, i - start + 1);
+    }
+    return ans;
   }
 
 }
--- src/test/java/spencercjh/problems/LongestContinuousIncreasingSubsequenceTest.java
+++ src/test/java/spencercjh/problems/LongestContinuousIncreasingSubsequenceTest.java
@@ -5,17 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class LongestContinuousIncreasingSubsequenceTest {
 
   @Inject
   private LongestContinuousIncreasingSubsequence solution;
-  
+
   @Test
   void findLengthOfLCIS() {
-    
+    assertEquals(3, solution.findLengthOfLCIS(new int[]{1, 3, 5, 4, 7}));
+    assertEquals(1, solution.findLengthOfLCIS(new int[]{2, 2, 2}));
   }
 
 }
