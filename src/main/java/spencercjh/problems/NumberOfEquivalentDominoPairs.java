2d9e04570277a699bd08619b591d0132e56672fc
spencercjh <shouspencercjh@foxmail.com>
2021-01-26 15:34:47 +0800

    1128

--- src/main/java/spencercjh/problems/NumberOfEquivalentDominoPairs.java
+++ src/main/java/spencercjh/problems/NumberOfEquivalentDominoPairs.java
@@ -10,8 +10,20 @@ import javax.inject.Singleton;
 @Singleton
 public class NumberOfEquivalentDominoPairs {
 
-  public int numEquivDominoPairs(int[][] dominoes) {
-    return 0;
-  }
+  public static final int MAX_VALUE = 100;
 
+  public int numEquivDominoPairs(int[][] dominoes) {
+    final int[] values = new int[MAX_VALUE];
+    int ans = 0;
+    for (int[] dominoe : dominoes) {
+      int value;
+      if (dominoe[0] < dominoe[1]) {
+        value = dominoe[0] * 10 + dominoe[1];
+      } else {
+        value = dominoe[1] * 10 + dominoe[0];
+      }
+      ans += values[value]++;
+    }
+    return ans;
+  }
 }
--- src/test/java/spencercjh/problems/NumberOfEquivalentDominoPairsTest.java
+++ src/test/java/spencercjh/problems/NumberOfEquivalentDominoPairsTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @MicronautTest
 class NumberOfEquivalentDominoPairsTest {
 
@@ -13,6 +15,7 @@ class NumberOfEquivalentDominoPairsTest {
 
   @Test
   void numEquivDominoPairs() {
+    assertEquals(1, solution.numEquivDominoPairs(new int[][]{{1, 2}, {2, 1}, {3, 4}, {5, 6}}));
   }
 
 }
