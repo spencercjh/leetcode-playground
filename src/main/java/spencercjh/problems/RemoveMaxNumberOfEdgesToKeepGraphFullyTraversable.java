8a23c08f97e3a9cdafaee206b6097c44b3e4708c
spencercjh <shouspencercjh@foxmail.com>
2021-01-27 10:34:14 +0800

    1579

--- src/main/java/spencercjh/problems/RemoveMaxNumberOfEdgesToKeepGraphFullyTraversable.java
+++ src/main/java/spencercjh/problems/RemoveMaxNumberOfEdgesToKeepGraphFullyTraversable.java
@@ -1,17 +1,82 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/remove-max-number-of-edges-to-keep-graph-fully-traversable/
  *
  * @author spencercjh
  */
 @Singleton
-public class RemoveMaxNumberOfEdgesToKeepGraphFullyTraversable{
+public class RemoveMaxNumberOfEdgesToKeepGraphFullyTraversable {
 
-    public int maxNumEdgesToRemove(int n, int[][] edges) {
+  public int maxNumEdgesToRemove(int n, int[][] edges) {
+    final UnionFind alice = new UnionFind(n);
+    final UnionFind bob = new UnionFind(n);
+    int ans = 0;
 
+    for (int[] edge : edges) {
+      edge[1]--;
+      edge[2]--;
     }
 
+    for (int[] edge : edges) {
+      if (edge[0] == 3) {
+        if (!alice.union(edge[1], edge[2])) {
+          ans++;
+        } else {
+          bob.union(edge[1], edge[2]);
+        }
+      }
+    }
+
+    for (int[] edge : edges) {
+      if (edge[0] == 1) {
+        if (!alice.union(edge[1], edge[2])) {
+          ans++;
+        }
+      } else if (edge[0] == 2) {
+        if (!bob.union(edge[1], edge[2])) {
+          ans++;
+        }
+      }
+    }
+
+    if (alice.count != 1 || bob.count != 1) {
+      return -1;
+    }
+    return ans;
+  }
+
+
+  private static class UnionFind {
+    private final int[] parents;
+    private int count;
+
+    public UnionFind(int n) {
+      parents = new int[n];
+      count = n;
+      for (int i = 0; i < parents.length; i++) {
+        parents[i] = i;
+      }
+    }
+
+    private int find(int n) {
+      if (n != parents[n]) {
+        parents[n] = find(parents[n]);
+      }
+      return parents[n];
+    }
+
+    public boolean union(int x, int y) {
+      x = find(x);
+      y = find(y);
+      if (x == y) {
+        return false;
+      }
+      parents[x] = y;
+      count--;
+      return true;
+    }
+  }
 }
--- src/test/java/spencercjh/problems/RemoveMaxNumberOfEdgesToKeepGraphFullyTraversableTest.java
+++ src/test/java/spencercjh/problems/RemoveMaxNumberOfEdgesToKeepGraphFullyTraversableTest.java
@@ -5,17 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class RemoveMaxNumberOfEdgesToKeepGraphFullyTraversableTest {
 
   @Inject
   private RemoveMaxNumberOfEdgesToKeepGraphFullyTraversable solution;
-  
+
   @Test
   void maxNumEdgesToRemove() {
-    
+    assertEquals(2, solution.maxNumEdgesToRemove(4, new int[][]{{3, 1, 2}, {3, 2, 3}, {1, 1, 3}, {1, 2, 4}, {1, 1, 2}, {2, 3, 4}}));
+    assertEquals(0, solution.maxNumEdgesToRemove(4, new int[][]{{3, 1, 2}, {3, 2, 3}, {1, 1, 4}, {2, 1, 4}}));
   }
 
 }
