c88726be8074a35317a0302e36b49bb71f4e6b18
spencercjh <shouspencercjh@foxmail.com>
2021-01-28 11:02:03 +0800

    724

--- src/main/java/spencercjh/problems/FindPivotIndex.java
+++ src/main/java/spencercjh/problems/FindPivotIndex.java
@@ -1,17 +1,32 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/find-pivot-index/
  *
  * @author spencercjh
  */
 @Singleton
-public class FindPivotIndex{
+public class FindPivotIndex {
 
-    public int pivotIndex(int[] nums) {
-
+  public int pivotIndex(int[] nums) {
+    if (nums == null || nums.length <= 1) {
+      return -1;
     }
 
+    // It takes longer to use stream api...
+    int total = 0;
+    for (int num : nums) {
+      total += num;
+    }
+
+    for (int i = 0, sum = 0; i < nums.length; i++) {
+      if (sum * 2 == total - nums[i]) {
+        return i;
+      }
+      sum += nums[i];
+    }
+    return -1;
+  }
 }
--- src/test/java/spencercjh/problems/FindPivotIndexTest.java
+++ src/test/java/spencercjh/problems/FindPivotIndexTest.java
@@ -5,17 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class FindPivotIndexTest {
 
   @Inject
   private FindPivotIndex solution;
-  
+
   @Test
   void pivotIndex() {
-    
+    assertEquals(3, solution.pivotIndex(new int[]{1, 7, 3, 6, 5, 6}));
+    assertEquals(-1, solution.pivotIndex(new int[]{1, 2, 3}));
   }
 
 }
