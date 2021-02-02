8468ad008040814d48aa9c7a3fdd4c9c12ab7b44
spencercjh <shouspencercjh@foxmail.com>
2021-02-02 16:05:46 +0800

    888

--- src/main/java/spencercjh/problems/FairCandySwap.java
+++ src/main/java/spencercjh/problems/FairCandySwap.java
@@ -10,8 +10,26 @@ import javax.inject.Singleton;
 @Singleton
 public class FairCandySwap {
 
-  public int[] fairCandySwap(int[] A, int[] B) {
+  private static final int MAX_VALUE = 100000;
+
+  public int[] fairCandySwap(int[] alice, int[] bob) {
+    int sumA = 0;
+    final boolean[] aliceExisted = new boolean[MAX_VALUE + 1];
+    for (int a : alice) {
+      aliceExisted[a] = true;
+      sumA += a;
+    }
+    int sumB = 0;
+    for (int b : bob) {
+      sumB += b;
+    }
+    final int delta = (sumA - sumB) / 2;
+    for (int b : bob) {
+      final int a = b + delta;
+      if (a >= 0 && a <= MAX_VALUE && aliceExisted[a]) {
+        return new int[]{a, b};
+      }
+    }
     return new int[]{};
   }
-
 }
--- src/test/java/spencercjh/problems/FairCandySwapTest.java
+++ src/test/java/spencercjh/problems/FairCandySwapTest.java
@@ -5,17 +5,20 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static spencercjh.AssertionUtils.assertArrayEquals;
 
 @MicronautTest
 class FairCandySwapTest {
 
   @Inject
   private FairCandySwap solution;
-  
+
   @Test
   void fairCandySwap() {
-    
+    assertArrayEquals(new int[]{1, 2}, solution.fairCandySwap(new int[]{1, 1}, new int[]{2, 2}));
+    assertArrayEquals(new int[]{1, 2}, solution.fairCandySwap(new int[]{1, 2}, new int[]{2, 3}));
+    assertArrayEquals(new int[]{2, 3}, solution.fairCandySwap(new int[]{2}, new int[]{1, 3}));
+    assertArrayEquals(new int[]{5, 4}, solution.fairCandySwap(new int[]{1, 2, 5}, new int[]{2, 4}));
   }
 
 }
