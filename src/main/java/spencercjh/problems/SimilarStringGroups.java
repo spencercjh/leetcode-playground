e967035da04fa36619a82af249456557a7fc83de
spencercjh <shouspencercjh@foxmail.com>
2021-02-02 15:47:00 +0800

    839

--- src/main/java/spencercjh/problems/SimilarStringGroups.java
+++ src/main/java/spencercjh/problems/SimilarStringGroups.java
@@ -9,9 +9,53 @@ import javax.inject.Singleton;
  */
 @Singleton
 public class SimilarStringGroups {
+  private int[] parents;
 
   public int numSimilarGroups(String[] strs) {
-    return 0;
+    parents = new int[strs.length];
+    for (int i = 0; i < parents.length; i++) {
+      parents[i] = i;
+    }
+
+    for (int i = 0; i < strs.length; i++) {
+      for (int j = i + 1; j < strs.length; j++) {
+        final int rootI = find(i);
+        final int rootJ = find(j);
+        if (rootI == rootJ) {
+          continue;
+        }
+        if (check(strs[i].toCharArray(), strs[j].toCharArray())) {
+          parents[rootI] = rootJ;
+        }
+      }
+    }
+
+    int answer = 0;
+    for (int i = 0; i < parents.length; i++) {
+      if (parents[i] == i) {
+        answer++;
+      }
+    }
+    return answer;
+  }
+
+  private int find(int n) {
+    if (n != parents[n]) {
+      parents[n] = find(parents[n]);
+    }
+    return parents[n];
+  }
+
+  private boolean check(char[] a, char[] b) {
+    int num = 0;
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] != b[i]) {
+        if (++num > 2) {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 
 }
--- src/test/java/spencercjh/problems/SimilarStringGroupsTest.java
+++ src/test/java/spencercjh/problems/SimilarStringGroupsTest.java
@@ -5,17 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class SimilarStringGroupsTest {
 
   @Inject
   private SimilarStringGroups solution;
-  
+
   @Test
   void numSimilarGroups() {
-    
+    assertEquals(2, solution.numSimilarGroups(new String[]{"tars", "rats", "arts", "star"}));
+    assertEquals(1, solution.numSimilarGroups(new String[]{"omv", "ovm"}));
   }
 
 }
