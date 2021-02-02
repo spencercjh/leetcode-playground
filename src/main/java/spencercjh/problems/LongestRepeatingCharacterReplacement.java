bf9b4ed711e00ae45eea93871e2a135a63b44304
spencercjh <shouspencercjh@foxmail.com>
2021-02-02 16:17:54 +0800

    424

--- src/main/java/spencercjh/problems/LongestRepeatingCharacterReplacement.java
+++ src/main/java/spencercjh/problems/LongestRepeatingCharacterReplacement.java
@@ -11,7 +11,17 @@ import javax.inject.Singleton;
 public class LongestRepeatingCharacterReplacement {
 
   public int characterReplacement(String s, int k) {
-    return 0;
+    final int[] num = new int[26];
+    int max = 0, left = 0, right = 0;
+    final char[] chars = s.toCharArray();
+    while (right < chars.length) {
+      max = Math.max(max, ++num[chars[right] - 'A']);
+      if (right - left + 1 - max > k) {
+        num[chars[left] - 'A']--;
+        left++;
+      }
+      right++;
+    }
+    return right - left;
   }
-
 }
--- src/test/java/spencercjh/problems/LongestRepeatingCharacterReplacementTest.java
+++ src/test/java/spencercjh/problems/LongestRepeatingCharacterReplacementTest.java
@@ -5,17 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class LongestRepeatingCharacterReplacementTest {
 
   @Inject
   private LongestRepeatingCharacterReplacement solution;
-  
+
   @Test
   void characterReplacement() {
-    
+    assertEquals(4, solution.characterReplacement("AABABBA", 1));
   }
 
 }
