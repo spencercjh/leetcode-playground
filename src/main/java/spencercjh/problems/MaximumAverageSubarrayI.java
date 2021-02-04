c6e6f240ad0e4d5c9e201112e7b1a246b1c92bf9
spencercjh <shouspencercjh@foxmail.com>
2021-02-04 11:38:51 +0800

    643

--- src/main/java/spencercjh/problems/MaximumAverageSubarrayI.java
+++ src/main/java/spencercjh/problems/MaximumAverageSubarrayI.java
@@ -1,17 +1,30 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/maximum-average-subarray-i/
  *
  * @author spencercjh
  */
 @Singleton
-public class MaximumAverageSubarrayI{
+public class MaximumAverageSubarrayI {
 
-    public double findMaxAverage(int[] nums, int k) {
-
+  public double findMaxAverage(int[] nums, int k) {
+    if (nums == null || nums.length == 0) {
+      return 0;
     }
+    // loop is obviously faster than stream api on LeetCode
+    int sum = 0;
+    for (int i = 0; i < k; i++) {
+      sum += nums[i];
+    }
+    int maxSum = sum;
+    for (int i = k; i < nums.length; i++) {
+      sum = sum - nums[i - k] + nums[i];
+      maxSum = Math.max(maxSum, sum);
+    }
+    return (double) maxSum / k;
+  }
 
 }
--- src/test/java/spencercjh/problems/MaximumAverageSubarrayITest.java
+++ src/test/java/spencercjh/problems/MaximumAverageSubarrayITest.java
@@ -5,17 +5,18 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class MaximumAverageSubarrayITest {
 
   @Inject
   private MaximumAverageSubarrayI solution;
-  
+
   @Test
   void findMaxAverage() {
-    
+    assertEquals(12.75, solution.findMaxAverage(new int[]{1, 12, -5, -6, 50, 3}, 4));
+    assertEquals(5, solution.findMaxAverage(new int[]{5}, 1));
   }
 
 }
