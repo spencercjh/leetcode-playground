e578040ce56ee069c01061c7fb9983644f2de29a
spencercjh <shouspencercjh@foxmail.com>
2021-02-05 15:25:17 +0800

    1208

--- src/main/java/spencercjh/problems/GetEqualSubstringsWithinBudget.java
+++ src/main/java/spencercjh/problems/GetEqualSubstringsWithinBudget.java
@@ -1,17 +1,27 @@
 package spencercjh.problems;
-           
+
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/get-equal-substrings-within-budget/
  *
  * @author spencercjh
  */
 @Singleton
-public class GetEqualSubstringsWithinBudget{
+public class GetEqualSubstringsWithinBudget {
 
-    public int equalSubstring(String s, String t, int maxCost) {
-
+  public int equalSubstring(String s, String t, int maxCost) {
+    int left = 0;
+    final char[] a = s.toCharArray();
+    final char[] b = t.toCharArray();
+    for (int i = 0, cost = 0; i < a.length; i++) {
+      cost += Math.abs(a[i] - b[i]);
+      if (cost > maxCost) {
+        cost -= Math.abs(a[left] - b[left]);
+        left++;
+      }
     }
+    return a.length - left;
+  }
 
 }
--- src/test/java/spencercjh/problems/GetEqualSubstringsWithinBudgetTest.java
+++ src/test/java/spencercjh/problems/GetEqualSubstringsWithinBudgetTest.java
@@ -5,17 +5,19 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @MicronautTest
 class GetEqualSubstringsWithinBudgetTest {
 
   @Inject
   private GetEqualSubstringsWithinBudget solution;
-  
+
   @Test
   void equalSubstring() {
-    
+    assertEquals(3, solution.equalSubstring("abcd", "bcdf", 3));
+    assertEquals(1, solution.equalSubstring("abcd", "cdef", 3));
+    assertEquals(1, solution.equalSubstring("abcd", "acde", 0));
   }
 
 }
