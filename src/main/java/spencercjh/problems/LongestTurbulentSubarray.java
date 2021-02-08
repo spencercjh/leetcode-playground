48271569f376053df3dbb0da9013f6b4decfb0af
spencercjh <shouspencercjh@foxmail.com>
2021-02-08 11:22:26 +0800

    978

--- src/main/java/spencercjh/problems/LongestTurbulentSubarray.java
+++ src/main/java/spencercjh/problems/LongestTurbulentSubarray.java
@@ -10,8 +10,22 @@ import javax.inject.Singleton;
 @Singleton
 public class LongestTurbulentSubarray {
 
-    public int maxTurbulenceSize(int[] arr) {
-        return 0;
+  public int maxTurbulenceSize(int[] arr) {
+    int answer = 1;
+    for (int i = 1, dp0 = 1, dp1 = 1; i < arr.length; i++) {
+      if (arr[i - 1] > arr[i]) {
+        dp0 = dp1 + 1;
+        dp1 = 1;
+      } else if (arr[i - 1] < arr[i]) {
+        dp1 = dp0 + 1;
+        dp0 = 1;
+      } else {
+        dp0 = 1;
+        dp1 = 1;
+      }
+      answer = Math.max(answer, dp0);
+      answer = Math.max(answer, dp1);
     }
-
+    return answer;
+  }
 }
