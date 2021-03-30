8be3c4afccbb582b98dba465a74af81e3382ba49
spencercjh <shouspencercjh@foxmail.com>
2021-03-30 13:46:09 +0800

    74

--- src/main/java/spencercjh/problems/SearchA2DMatrix.java
+++ src/main/java/spencercjh/problems/SearchA2DMatrix.java
@@ -2,11 +2,48 @@ package spencercjh.problems;
 
 /**
  * https://leetcode-cn.com/problems/search-a-2d-matrix/
- * 
+ *
  * @author spencercjh
  */
-class SearchA2DMatrix {
-    public boolean searchMatrix(int[][] matrix, int target) {
-
+public class SearchA2DMatrix {
+  public boolean searchMatrix(int[][] matrix, int target) {
+    if (matrix == null || matrix.length == 0) {
+      return false;
     }
+    final int rowIndex = firstColumnBinarySearch(matrix, target);
+    if (rowIndex < 0) {
+      return false;
+    }
+    return rowBinarySearch(matrix[rowIndex], target);
+  }
+
+  private int firstColumnBinarySearch(int[][] matrix, int target) {
+    int left = -1;
+    int right = matrix.length - 1;
+    while (left < right) {
+      final int middle = left + (right - left + 1) / 2;
+      if (matrix[middle][0] > target) {
+        right = middle - 1;
+      } else {
+        left = middle;
+      }
+    }
+    return left;
+  }
+
+  private boolean rowBinarySearch(int[] row, int target) {
+    int left = 0;
+    int right = row.length - 1;
+    while (left <= right) {
+      final int middle = left + (right - left) / 2;
+      if (row[middle] == target) {
+        return true;
+      } else if (row[middle] > target) {
+        right = middle - 1;
+      } else {
+        left = middle + 1;
+      }
+    }
+    return false;
+  }
 }
