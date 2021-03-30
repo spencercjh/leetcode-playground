05204f1fd02b20dd079b1ccbfc9981950cb7c19a
spencercjh <shouspencercjh@foxmail.com>
2021-03-30 14:02:34 +0800

    190

--- src/main/java/spencercjh/problems/ReverseBits.java
+++ src/main/java/spencercjh/problems/ReverseBits.java
@@ -2,12 +2,22 @@ package spencercjh.problems;
 
 /**
  * https://leetcode-cn.com/problems/reverse-bits/
- * 
+ *
  * @author spencercjh
  */
 public class ReverseBits {
-    // you need treat n as an unsigned value
-    public int reverseBits(int n) {
-        
+
+  /**
+   * you need treat n as an unsigned value
+   */
+  public int reverseBits(int n) {
+    int answer = 0;
+    for (int i = 0; i < Integer.SIZE; i++) {
+      // get the last bit and shift left it to high position
+      answer |= (n & 1) << (Integer.SIZE - 1 - i);
+      // shift right n 1 bit for next iteration
+      n >>>= 1;
     }
+    return answer;
+  }
 }
