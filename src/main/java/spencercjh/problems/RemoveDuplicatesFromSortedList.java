8f155e033abbd5b63f51755114075ad5ed56a3d6
spencercjh <shouspencercjh@foxmail.com>
2021-04-02 12:31:28 +0800

    83

--- src/main/java/spencercjh/problems/RemoveDuplicatesFromSortedList.java
+++ src/main/java/spencercjh/problems/RemoveDuplicatesFromSortedList.java
@@ -1,20 +1,30 @@
 package spencercjh.problems;
-           
+
 import spencercjh.ListNode;
 
 import javax.inject.Singleton;
-            
+
 /**
  * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
  *
  * @author spencercjh
  */
 @Singleton
-public class RemoveDuplicatesFromSortedList{
+public class RemoveDuplicatesFromSortedList {
 
-    public ListNode deleteDuplicates(ListNode head) {
-
-        return head;
+  public ListNode deleteDuplicates(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
     }
+    ListNode current = head;
+    while (current.next != null) {
+      if (current.val == current.next.val) {
+        current.next = current.next.next;
+      } else {
+        current = current.next;
+      }
+    }
+    return head;
+  }
 
 }
--- src/test/java/spencercjh/problems/RemoveDuplicatesFromSortedListTest.java
+++ src/test/java/spencercjh/problems/RemoveDuplicatesFromSortedListTest.java
@@ -2,20 +2,22 @@ package spencercjh.problems;
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
+import spencercjh.ListNode;
 
 import javax.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static spencercjh.AssertionUtils.assertArrayEquals;
 
 @MicronautTest
 class RemoveDuplicatesFromSortedListTest {
 
   @Inject
   private RemoveDuplicatesFromSortedList solution;
-  
+
   @Test
   void deleteDuplicates() {
-    
+    assertArrayEquals(ListNode.parseListNodeToArray(solution.deleteDuplicates(
+      ListNode.buildListNodeWithValues(1, 1, 2, 2, 3, 3, 4))), new int[]{1, 2, 3, 4});
   }
 
 }
