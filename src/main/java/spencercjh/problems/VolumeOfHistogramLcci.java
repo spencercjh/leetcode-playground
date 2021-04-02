3bdf878a95ffa68d9e53a3a239fd47d9b79c996a
spencercjh <shouspencercjh@foxmail.com>
2021-04-02 08:52:03 +0800

    面试题 17.21

--- src/main/java/spencercjh/problems/VolumeOfHistogramLcci.java
+++ src/main/java/spencercjh/problems/VolumeOfHistogramLcci.java
@@ -2,11 +2,28 @@ package spencercjh.problems;
 
 /**
  * https://leetcode-cn.com/problems/volume-of-histogram-lcci/
- * 
+ *
  * @author spencercjh
  */
 class VolumeOfHistogramLcci {
-    public int trap(int[] height) {
-
+  public int trap(int[] height) {
+    if (height == null || height.length == 0) {
+      return 0;
     }
+    int left = 0, right = height.length - 1;
+    int leftMax = 0, rightMax = 0;
+    int answer = 0;
+    while (left < right) {
+      leftMax = Math.max(height[left], leftMax);
+      rightMax = Math.max(height[right], rightMax);
+      if (height[left] < height[right]) {
+        answer += leftMax - height[left];
+        left++;
+      } else {
+        answer += rightMax - height[right];
+        right--;
+      }
+    }
+    return answer;
+  }
 }
