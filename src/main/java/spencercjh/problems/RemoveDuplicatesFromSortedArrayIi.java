9f21c1e6362ea64cf0adde3b3f3bf656a1e3462f
spencercjh <shouspencercjh@foxmail.com>
2021-04-07 14:23:50 +0800

    80

--- src/main/java/spencercjh/problems/RemoveDuplicatesFromSortedArrayIi.java
+++ src/main/java/spencercjh/problems/RemoveDuplicatesFromSortedArrayIi.java
@@ -2,11 +2,27 @@ package spencercjh.problems;
 
 /**
  * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii/
- * 
+ *
  * @author spencercjh
  */
 class RemoveDuplicatesFromSortedArrayIi {
-    public int removeDuplicates(int[] nums) {
 
+  private static final int THRESHOLD = 2;
+
+  public int removeDuplicates(int[] nums) {
+    if (nums == null) {
+      return 0;
     }
+    if (nums.length <= THRESHOLD) {
+      return nums.length;
+    }
+    int slow = THRESHOLD, fast = THRESHOLD;
+    while (fast < nums.length) {
+      if (nums[slow - THRESHOLD] != nums[fast]) {
+        nums[slow++] = nums[fast];
+      }
+      fast++;
+    }
+    return slow;
+  }
 }
