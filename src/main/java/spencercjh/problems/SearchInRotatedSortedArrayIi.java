0fdc84a1c6b9f42d4365f2856c7fee6d99d4b0f6
spencercjh <shouspencercjh@foxmail.com>
2021-04-07 14:32:42 +0800

    81

--- src/main/java/spencercjh/problems/SearchInRotatedSortedArrayIi.java
+++ src/main/java/spencercjh/problems/SearchInRotatedSortedArrayIi.java
@@ -1,12 +1,23 @@
 package spencercjh.problems;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode-cn.com/problems/search-in-rotated-sorted-array-ii/
- * 
+ *
  * @author spencercjh
  */
 class SearchInRotatedSortedArrayIi {
-    public boolean search(int[] nums, int target) {
-
+  public boolean search(int[] nums, int target) {
+    if (nums == null || nums.length == 0) {
+      return false;
     }
+    if (nums.length == 1) {
+      return nums[0] == target;
+    }
+    return Arrays.stream(nums)
+      .filter(x -> x == target)
+      .findFirst()
+      .isPresent();
+  }
 }
