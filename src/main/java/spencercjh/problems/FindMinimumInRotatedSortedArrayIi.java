e05ce59c11e6f3ca8083b5572d49cfcd79dae3a8
spencercjh <shouspencercjh@foxmail.com>
2021-04-09 09:07:43 +0800

    154

--- src/main/java/spencercjh/problems/FindMinimumInRotatedSortedArrayIi.java
+++ src/main/java/spencercjh/problems/FindMinimumInRotatedSortedArrayIi.java
@@ -2,11 +2,23 @@ package spencercjh.problems;
 
 /**
  * https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/
- * 
+ *
  * @author spencercjh
  */
 class FindMinimumInRotatedSortedArrayIi {
-    public int findMin(int[] nums) {
-
+  public int findMin(int[] nums) {
+    int left = 0;
+    int right = nums.length - 1;
+    while (left < right) {
+      final int middle = left + (right - left) / 2;
+      if (nums[middle] < nums[right]) {
+        right = middle;
+      } else if (nums[middle] > nums[right]) {
+        left = middle + 1;
+      } else {
+        right -= 1;
+      }
     }
+    return nums[left];
+  }
 }
