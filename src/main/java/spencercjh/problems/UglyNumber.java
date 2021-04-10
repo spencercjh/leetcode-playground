bb81da42cc48d3678e01d9c0fa4a1f6daaa84175
spencercjh <shouspencercjh@foxmail.com>
2021-04-10 09:26:16 +0800

    263

--- src/main/java/spencercjh/problems/UglyNumber.java
+++ src/main/java/spencercjh/problems/UglyNumber.java
@@ -2,11 +2,21 @@ package spencercjh.problems;
 
 /**
  * https://leetcode-cn.com/problems/ugly-number/
- * 
+ *
  * @author spencercjh
  */
 class UglyNumber {
-    public boolean isUgly(int n) {
+  private static final int[] FACTORS = new int[]{2, 3, 5};
 
+  public boolean isUgly(int n) {
+    if (n <= 0) {
+      return false;
     }
+    for (int factor : FACTORS) {
+      while (n % factor == 0) {
+        n /= factor;
+      }
+    }
+    return n == 1;
+  }
 }
