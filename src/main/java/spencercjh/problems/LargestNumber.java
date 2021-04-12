4bbca6a8f25e131b8cc86f8d626d39e4316c5b38
spencercjh <shouspencercjh@foxmail.com>
2021-04-12 13:47:24 +0800

    179

--- src/main/java/spencercjh/problems/LargestNumber.java
+++ src/main/java/spencercjh/problems/LargestNumber.java
@@ -1,12 +1,24 @@
 package spencercjh.problems;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode-cn.com/problems/largest-number/
- * 
+ *
  * @author spencercjh
  */
 class LargestNumber {
-    public String largestNumber(int[] nums) {
 
+  private static final String ZERO = "0";
+
+  public String largestNumber(int[] nums) {
+    final String[] numStrings = Arrays.stream(nums)
+      .mapToObj(String::valueOf)
+      .sorted((a, b) -> (b + a).compareTo(a + b))
+      .toArray(String[]::new);
+    if (ZERO.equals(numStrings[0])) {
+      return ZERO;
     }
+    return String.join("", numStrings);
+  }
 }
